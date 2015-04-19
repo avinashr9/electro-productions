9bf6dc72dfee8a26d13adf092c9b0661365884ff
avinaashchowdry <raaviavinash8072@gmail.com>
2015-04-19 18:44:03 -0400

    Added animated effect to display and hide services

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
 $(document).ready(function(){
     $(".cursor_style").click(function(){
-        if($(this).next().hasClass("hide_service")) {
-			$(this).next().removeClass("hide_service").addClass("display_service");
-		}
-		else{
-			$(this).next().addClass("hide_service").removeClass("display_service");
-		}
+		$(this).next().slideToggle("slow");
     });
 });
