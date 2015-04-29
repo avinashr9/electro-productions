54d7b88f0dc3ebb46cd701788ecc4bbbacc2ef40
avinaashchowdry <raaviavinash8072@gmail.com>
2015-04-29 16:55:53 -0400

    Made changes to header o make it responsive

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
     $(".cursor_style").click(function(){
 		$(this).next().slideToggle("slow");
+		/*if ($(window).width() <= 800){	
+		  if(this.document
+		}
+		else {
+		}*/
     });
 });
