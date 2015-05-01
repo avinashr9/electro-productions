f4eab5c1f0e9184a904e55d95d623ff74122f955
avinaashchowdry <raaviavinash8072@gmail.com>
2015-05-01 17:06:44 -0400

    Removed unused code from the script file

--- js/script.js
+++ js/script.js
@@ -1,60 +1,51 @@
 $(document).ready(function(){
     $(".cursor_style").click(function(){
 		$(this).next().slideToggle("slow");
-		/*if ($(window).width() <= 800){	
-		  if(this.document
-		}
-		else {
-		}*/
     });
-	
+
 	/* video pause */
 	$("#video1").click( function(){
-	  
        $(this).get(0).paused ? $(this).get(0).play() : $(this).get(0).pause();
 	});
-	
+
 	/* fancybox video*/
 	$('.fancybox').fancybox();
 
-			/*
-			 *  Media helper. Group items, disable animations, hide arrows, enable media and button helpers.
-			*/
-			$('.fancybox-media')
-				.attr('rel', 'media-gallery')
-				.fancybox({
-					openEffect : 'none',
-					closeEffect : 'none',
-					prevEffect : 'none',
-					nextEffect : 'none',
+	/*
+	 *  Media helper. Group items, disable animations, hide arrows, enable media and button helpers.
+	 */
+	$('.fancybox-media')
+		.attr('rel', 'media-gallery')
+		.fancybox({
+			openEffect : 'none',
+			closeEffect : 'none',
+			prevEffect : 'none',
+			nextEffect : 'none',
 
-					arrows : false,
-					helpers : {
-						media : {},
-						buttons : {}
-					}
-				});
-				
-	
+			arrows : false,
+			helpers : {
+				media : {},
+				buttons : {}
+			}
+		});
+
     /* Jquery page scroll*/	
 	$('#nav').onePageNav({
-    currentClass: 'current',
-    changeHash: false,
-    scrollSpeed: 750,
-    scrollThreshold: 0.5,
-    filter: '',
-    easing: 'swing',
-    begin: function() {
+      currentClass: 'current',
+      changeHash: false,
+      scrollSpeed: 750,
+      scrollThreshold: 0.5,
+      filter: '',
+      easing: 'swing',
+      begin: function() {
         //I get fired when the animation is starting
-    },
-    end: function() {
+      },
+      end: function() {
         //I get fired when the animation is ending
-    },
-    scrollChange: function($currentListItem) {
+      },
+      scrollChange: function($currentListItem) {
         //I get fired when you enter a section and I pass the list item of the section
-    }
-});
-	
-});
+      }
+    });
 
- 
+}); 
