5e2cde54999f3b5215bc0c8a88ec7f4974e51f8b
gudipuds <sravya.gudipudi@gmail.com>
2015-05-01 16:27:58 -0400

    Added page scroll and demo reel button

--- js/script.js
+++ js/script.js
@@ -34,6 +34,26 @@ $(document).ready(function(){
 						buttons : {}
 					}
 				});
+				
+	
+    /* Jquery page scroll*/	
+	$('#nav').onePageNav({
+    currentClass: 'current',
+    changeHash: false,
+    scrollSpeed: 750,
+    scrollThreshold: 0.5,
+    filter: '',
+    easing: 'swing',
+    begin: function() {
+        //I get fired when the animation is starting
+    },
+    end: function() {
+        //I get fired when the animation is ending
+    },
+    scrollChange: function($currentListItem) {
+        //I get fired when you enter a section and I pass the list item of the section
+    }
+});
 	
 });
 
