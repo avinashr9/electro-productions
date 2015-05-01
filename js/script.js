b8e9057db58e08ef5d49900ab9293eb3a4976685
gudipuds <sravya.gudipudi@gmail.com>
2015-05-01 01:16:47 -0400

    added background video and logo

--- js/script.js
+++ js/script.js
@@ -7,4 +7,34 @@ $(document).ready(function(){
 		else {
 		}*/
     });
+	
+	/* video pause */
+	$("#video1").click( function(){
+	  
+       $(this).get(0).paused ? $(this).get(0).play() : $(this).get(0).pause();
+	});
+	
+	/* fancybox video*/
+	$('.fancybox').fancybox();
+
+			/*
+			 *  Media helper. Group items, disable animations, hide arrows, enable media and button helpers.
+			*/
+			$('.fancybox-media')
+				.attr('rel', 'media-gallery')
+				.fancybox({
+					openEffect : 'none',
+					closeEffect : 'none',
+					prevEffect : 'none',
+					nextEffect : 'none',
+
+					arrows : false,
+					helpers : {
+						media : {},
+						buttons : {}
+					}
+				});
+	
 });
+
+ 
